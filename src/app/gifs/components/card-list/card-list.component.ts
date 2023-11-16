7886fc44832bba52e685ee317af6faf7d3cb2291
Daniel Pineda <danielpineda.profe@gmail.com>
2023-11-16 13:15:06 -0500

    Sidebar terminado, searchbox comenzado

---
+++ src/app/gifs/components/card-list/card-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gifs-card-list',
+  templateUrl: './card-list.component.html',
+  styleUrls: ['./card-list.component.css']
+})
+export class CardListComponent {
+
+/*  public gifsList: string [] = ['Drake y Josh', 'I Carly', 'Mr Young', 'The Walking Dead', 'The Big Ban Theory'] */
+
+}
